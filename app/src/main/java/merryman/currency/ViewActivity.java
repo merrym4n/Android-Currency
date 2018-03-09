070cf57482fc2bd6dd5c1c46c62fbbc7775649ed
merrym4n <merrym4n@gmail.com>
2018-03-09 23:31:31 +0900

    backup

---
+++ app/src/main/java/merryman/currency/ViewActivity.java
@@ -0,0 +1,13 @@
+package merryman.currency;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+    }
+}
